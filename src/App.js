0becd96515582e75e26790a5b5adf94d56ff78fa
junek3 <rlarudrms28@gmail.com>
2018-08-12 21:50:41 +0900

    Loading States + Smart, Dumb Components

--- src/App.js
+++ src/App.js
@@ -2,32 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies=[
-  {
-    title:"Matrix",
-    poster:"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-  },
-  {
-    title:"Full Metal Jacket",
-    poster:"https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL._SY445_.jpg"
-  },
-  {
-    title:"Old Boy",
-    poster:"http://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg"
-  },
-  {
-    title:"Star Wars",
-    poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
-  }
-]
-
 class App extends Component {
-  render() {
-    return (
+
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  
+  state = {};
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies:[
+          {
+            title:"Matrix",
+            poster:"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+          },
+          {
+            title:"Full Metal Jacket",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL._SY445_.jpg"
+          },
+          {
+            title:"Old Boy",
+            poster:"http://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg"
+          },
+          {
+            title:"Star Wars",
+            poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+          },
+          {
+            title:"Mr.SunShine",
+            poster:"https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/2sNk/image/GSp4IeruRwm3qKN2E2jmsEz8v-Y.jpg"
+          }
+        ]
+      })
+    }, 5000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
+  render(){
+    return(
       <div className="App">
-       {movies.map((movie, index) => {
-         return <Movie title={movie.title} poster={movie.poster} key={index} />
-       })}
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,36 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-
-class Movie extends Component{
-
-    render(){
-        return(
-            <div>
-            <MoviePoster poster={this.props.poster} />
-            <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-Movie.propTypes = {
-    title: PropTypes.string.isRequired,
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
+}
+
+Movie.PropTypes = {
+    title: PropTypes.string.isRequired ,
     poster: PropTypes.string.isRequired
 }
 
-class MoviePoster extends Component{
-
-    render(){
-        return(
-            <img src={this.props.poster} alt="Movie Poster" />
-        )
-    }
-}
-
-MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired,
+MoviePoster.PropTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
